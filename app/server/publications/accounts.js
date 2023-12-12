58efde1ff6cf7eb712646cdefe93268bab2db23c
scn1901 <scnakamura2301@gmail.com>
2023-12-12 00:16:28 -1000

    minor conflicts

--- app/server/publications/accounts.js
+++ app/server/publications/accounts.js
@@ -3,8 +3,4 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('userAccounts', function() {
   return Meteor.users.find({}, { fields: { 'email': 1, 'username': 1, '_id': 1, }})
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 51dc0cb65723b70dbde163439f51145c91e997b8
